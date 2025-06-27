7ca9bb542ffffe416b52d4b40001daf247ef951e
topikk <118111616+taufiqpr@users.noreply.github.com>
2025-06-27 19:07:37 +0700

    new depende

--- app.py
+++ app.py
@@ -29,58 +29,6 @@ client = MongoClient(MONGO_URI)
 db = client["predict"]
 fruits_collection = db["fruits"]
 
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-model = YOLO("best.pt")
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    print("Menerima permintaan dari Flutter...")
-
-    if 'image' not in request.files:
-        print("❌ Tidak ada gambar ditemukan.")
-        return jsonify({"error": "No image uploaded"}), 400
-
-    image = request.files['image']
-    filename = f"{uuid.uuid4()}.jpg"
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
-    image.save(filepath)
-    print(f"📂 Gambar disimpan: {filepath}")
-
-    try:
-        results = model.predict(source=filepath, conf=0.3)
-
-        pred = results[0]
-        boxes = pred.boxes
-        names = model.names
-
-        detected_objects = []
-        for box in boxes:
-            cls_id = int(box.cls[0])
-            confidence = float(box.conf[0])
-            bbox = box.xyxy[0].tolist()  
-
-            detected_objects.append({
-                "class": names[cls_id],
-                "confidence": round(confidence, 2),
-                "bbox": [round(x, 2) for x in bbox]
-            })
-
-        print(f"Deteksi selesai: {len(detected_objects)} objek ditemukan.")
-
-        return jsonify({
-            "message": "Prediction success",
-            "detections": detected_objects
-        })
-
-    except Exception as e:
-        print(f"Error saat prediksi: {e}")
-        return jsonify({"error": str(e)}), 500
-
-    finally:
-        if os.path.exists(filepath):
-            os.remove(filepath)
 
 @app.route('/fruits', methods=['POST'])
 @jwt_required()
